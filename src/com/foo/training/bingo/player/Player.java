2b7f96c18693894b3242855c293de35b40dfcff1
kawakamimanabu <mnvkawa@gmail.com>
2016-04-19 09:19:58 +0900

    パッケージ名を変えました。

---
+++ src/com/foo/training/bingo/player/Player.java
@@ -0,0 +1,21 @@
+package com.foo.training.bingo.player;
+
+import com.wingdev.training.bingo.base.BaseBingoPlayer;
+
+public class Player extends BaseBingoPlayer {
+
+	public Player(String name, String[][] card) {
+		super(name, card);
+	}
+
+	@Override
+	public void shoutReach() {
+		System.out.println("...");
+	}
+
+	@Override
+	public void shoutBingo() {
+		System.out.println("Bi, Bi, BINGOOOOO!!!!!");
+	}
+
+}
